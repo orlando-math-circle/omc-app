1e0701229ccf0df35d957dd8cf3506d9e989c924
John Gilbert <gilbert.johnt@gmail.com>
2020-07-10 21:24:58 -0400

    refactor: remove frontend from this repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
--- README.md
+++
@@ -1,7 +0,0 @@
-# Spring2020-Group10
-
-Event calendar system with a native app-like experience for Orlando Math Circle. Spring - Fall 2020; Group 10.
-
-## Developer Setup
-
-Not yet implemented.
