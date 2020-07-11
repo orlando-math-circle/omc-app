fe796ac0d0483d621e151f0a9ecab9e16f68c0bb
John Gilbert <gilbert.johnt@gmail.com>
2020-07-11 18:17:16 -0400

    refactor: use new entity globs so less touching mikro-orm config

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -2,14 +2,13 @@ import { NotFoundException } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { Options, ReflectMetadataProvider } from 'mikro-orm';
 import * as path from 'path';
-import { Account } from './src/account/account.entity';
-import { User } from './src/user/user.entity';
 
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 export const config: Options = {
   type: 'postgresql',
-  entities: [Account, User],
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
   dbName: process.env.DATABASE_NAME || 'omc',
   user: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASS || 'postgres',
