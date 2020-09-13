9f90b4cd2edc646d4b188ee766423a803b17eda1
duckies <duckies@users.noreply.github.com>
2020-09-13 07:18:56 +0000

    deploy: 72018c19cca4d9c83d86ac4912850519ce68fc80

--- src/auth/decorators/inject-ac.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { Inject } from '@nestjs/common';
-import { ACCESS_CONTROL_TOKEN } from '../../app.constants';
-
-export const InjectAC = () => Inject(ACCESS_CONTROL_TOKEN);
--- src/event/interfaces/hydrated-event.interface.ts
+++
@@ -1,3 +0,0 @@
-import { Event } from '../event.entity';
-
-export type HydratedEvent = Omit<Event, 'id' | 'rrule'>;
