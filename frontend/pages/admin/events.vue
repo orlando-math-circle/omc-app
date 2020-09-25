04d896fc49b2a539837a6460f13413edf00d8fed
John Gilbert <gilbert.johnt@gmail.com>
2020-09-25 17:25:32 -0400

    feat(frontend): add incomplete admin calendar view

--- frontend/pages/admin/events.vue
+++ frontend/pages/admin/events.vue
@@ -1,12 +1,55 @@
 <template>
-  <h2>Events</h2>
+  <div>
+    <v-row>
+      <v-col cols="auto"
+        ><v-select
+          v-model="type"
+          :items="calendar.types"
+          hide-details
+          solo
+        ></v-select
+      ></v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-calendar :events="events" :type="calendar.type"></v-calendar>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import Vue from 'vue'
+import { COOKIE_CALENDAR_TYPE } from '~/utils/constants'
 
-@Component({
+export default Vue.extend({
   layout: 'admin',
+  data() {
+    return {
+      calendar: {
+        types: [
+          { text: 'Month', value: 'month' },
+          { text: 'Week', value: 'week' },
+          { text: 'Day', value: 'day' },
+          { text: '4 Days', value: '4day' },
+        ],
+      },
+    }
+  },
+  computed: {
+    type: {
+      get() {
+        return this.$store.state.auth.settings.calendarType
+      },
+      set(calendarType: string) {
+        this.$store.commit('auth/SET_SETTINGS', { calendarType })
+        this.$cookies.set(COOKIE_CALENDAR_TYPE, calendarType)
+      },
+    },
+    events() {
+      return this.$store.getters['events/events']
+    },
+  },
 })
-export default class AdminEventsPage extends Vue {}
 </script>
