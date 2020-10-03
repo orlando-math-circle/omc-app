690864a2ce2cc34f2b4daac1d9b6412d486534a7
John Gilbert <gilbert.johnt@gmail.com>
2020-10-02 22:39:51 -0400

    feat(backend): catch PostgreSQL constraint violations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { PORT } from './app.constants';
 import { AppModule } from './app.module';
 import { JsonWebTokenFilter } from './auth/filters/jwt.filter';
+import { MikroORMConstraintExceptionFilter } from './shared/errors/mikro-orm.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +19,10 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new JsonWebTokenFilter());
+  app.useGlobalFilters(
+    new JsonWebTokenFilter(),
+    new MikroORMConstraintExceptionFilter(),
+  );
 
   app.enableCors();
 
