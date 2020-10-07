4713e34bcc7363f69c8b178c9a62951372fc777f
John Gilbert <gilbert.johnt@gmail.com>
2020-10-06 20:56:44 -0400

    test: add missing event-registration test fixes

--- backend/test/event-registration.e2e-spec.ts
+++ backend/test/event-registration.e2e-spec.ts
@@ -6,17 +6,23 @@ import { Test } from '@nestjs/testing';
 import request from 'supertest';
 import { AccountModule } from '../src/account/account.module';
 import { testSchema } from '../src/app.config';
+import { Roles } from '../src/app.roles';
 import { AuthModule } from '../src/auth/auth.module';
 import { JsonWebTokenFilter } from '../src/auth/filters/jwt.filter';
 import { CourseModule } from '../src/course/course.module';
+import { CreateCourseDto } from '../src/course/dto/create-course.dto';
+import { LatePaymentType } from '../src/course/enums/late-payment-type.enum';
+import { PaymentType } from '../src/course/enums/payment-type.enum';
 import { EmailModule } from '../src/email/email.module';
 import { EventRegistrationModule } from '../src/event-registration/event-registration.module';
 import { EventModule } from '../src/event/event.module';
+import { CreateProjectDto } from '../src/project/dto/create-project.dto';
+import { ProjectModule } from '../src/project/project.module';
 import { MikroORMConstraintExceptionFilter } from '../src/shared/errors/mikro-orm.exception';
 import { SearchPipe } from '../src/shared/pipes/search.pipe';
 import { SortingPipe } from '../src/shared/pipes/sorting.pipe';
 import { UserModule } from '../src/user/user.module';
-import { createAccountFixture } from './fixtures/user.fixture';
+import { UserFixtures } from './fixtures/user.fixture';
 import { MikroORMTestingConfig } from './mikro-orm.test-config';
 
 describe.only('Event Registrations', () => {
@@ -27,6 +33,7 @@ describe.only('Event Registrations', () => {
    * Testing Data
    */
 
+  let userFixtures: UserFixtures;
   let token: string;
 
   beforeAll(async () => {
@@ -43,12 +50,15 @@ describe.only('Event Registrations', () => {
         AuthModule,
         EventModule,
         CourseModule,
+        ProjectModule,
         EventRegistrationModule,
       ],
     }).compile();
 
     app = moduleRef.createNestApplication();
     orm = moduleRef.get<MikroORM>(MikroORM);
+
+    userFixtures = new UserFixtures(app, orm);
 
     const generator = orm.getSchemaGenerator();
     await generator.ensureDatabase();
@@ -76,7 +86,7 @@ describe.only('Event Registrations', () => {
      * Seeding Data
      */
 
-    token = await createAccountFixture(app);
+    token = await userFixtures.createAccount();
   });
 
   afterEach(() => {
@@ -100,6 +110,58 @@ describe.only('Event Registrations', () => {
         .post('/registration/order/create/1')
         .set('Authorization', `Bearer ${token}`)
         .expect(403);
+    });
+
+    it('should throw 400 for unknown account users on non-admins', async () => {
+      await userFixtures.setUserRoles(1, [Roles.ADMIN]);
+
+      const createProjectDto: CreateProjectDto = {
+        name: 'TestProject',
+        description: 'First Fixture',
+      };
+
+      const project = await request(app.getHttpServer())
+        .post('/project')
+        .send(createProjectDto)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(201);
+
+      expect(project.body).toBeDefined();
+      expect(project.body).toEqual({
+        id: 1,
+        name: 'TestProject',
+        description: 'First Fixture',
+        courses: [],
+        events: [],
+      });
+
+      const createCourseDto: CreateCourseDto = {
+        name: `TestCourse:${new Date().getTime()}`,
+        description: 'First Fixture',
+        paymentType: PaymentType.ALL,
+        latePaymentType: LatePaymentType.DENY,
+        project: project.body.id,
+        fee: '12.34',
+      };
+
+      const course = await request(app.getHttpServer())
+        .post('/course')
+        .send(createCourseDto)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(201);
+
+      expect(course.body).toBeDefined();
+      expect(course.body).toEqual({
+        id: 1,
+        name: createCourseDto.name,
+        description: 'First Fixture',
+        paymentType: PaymentType.ALL,
+        latePaymentType: LatePaymentType.DENY,
+        project: 1,
+        fee: '12.34',
+        events: [],
+        invoices: [],
+      });
     });
   });
 });
