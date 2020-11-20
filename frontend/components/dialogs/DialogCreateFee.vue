7d3228b47363d3a49feef327dbe0bcc1d48a9573
John Gilbert <gilbert.johnt@gmail.com>
2020-11-20 04:18:56 -0500

    feat: add incomplete fee dialog

---
+++ frontend/components/dialogs/DialogCreateFee.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-dialog v-model="dialog" max-width="550">
+    <v-card>
+      <v-toolbar flat>
+        <v-toolbar-title>Event Fee</v-toolbar-title>
+      </v-toolbar>
+
+      <v-card-text>
+        <v-text-field-validated
+          v-model="dto.amount"
+          label="Event Fee"
+          type="number"
+        />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class DialogCreateFee extends Vue {
+  dialog = false
+
+  dto = {
+    amount: '',
+  }
+}
+</script>
