e0da49f9aa844608c07566bbe76c4cd6b9bd217e
Seikcud <gilbert.johnt@gmail.com>
2021-02-26 06:17:41 -0500

    feat(docs): use docus instead of nuxt content docs

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,14 +1,12 @@
-import theme from "@nuxt/content-theme-docs";
+import { withDocus } from "docus";
 
-export default theme({
-  server: {
-    port: 4000
+export default withDocus({
+  docus: {
+    colors: {
+      primary: "#E25F55"
+    }
   },
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN
-  },
-  loading: { color: "#ff4785" },
-  router: {
-    base: process.env.DEPLOY_ENV === "GH_PAGES" ? "/omc-app" : "/"
   }
 });
