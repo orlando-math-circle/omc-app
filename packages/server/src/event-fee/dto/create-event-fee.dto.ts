1c95854c9c6532e8c9bf3945f7c11ac4e27feb1c
Seikcud <gilbert.johnt@gmail.com>
2021-04-20 19:21:34 -0400

    fix(server): require 2 decimal places on fees

--- packages/server/src/event-fee/dto/create-event-fee.dto.ts
+++ packages/server/src/event-fee/dto/create-event-fee.dto.ts
@@ -1,6 +1,7 @@
-import { IsNumberString, IsOptional } from 'class-validator';
+import { IsDecimal, IsNumberString, IsOptional } from 'class-validator';
 
 export class CreateEventFeeDto {
+  @IsDecimal({ decimal_digits: '2', force_decimal: true })
   @IsNumberString()
   readonly amount!: string;
 
