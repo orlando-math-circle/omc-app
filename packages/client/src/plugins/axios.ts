bf2d8467064c733810894994e10816f52d0d21b9
Seikcud <gilbert.johnt@gmail.com>
2021-05-19 02:31:40 -0400

    refactor(client): tidy plugins

--- packages/client/src/plugins/axios.ts
+++ packages/client/src/plugins/axios.ts
@@ -1,12 +1,18 @@
 import { defineNuxtPlugin } from '@nuxtjs/composition-api'
 import { useAuth } from '@/store/useAuth'
 
-export default defineNuxtPlugin(({ $axios, redirect, pinia }) => {
-  const authStore = useAuth(pinia)
+/**
+ * Plugin: Axios Interceptor
+ *
+ * If any API request results in a `401 Unauthorized` error
+ * there is a credential issue, this plugin will logout
+ * the user and remove any associated data.
+ */
+
+export default defineNuxtPlugin(({ $axios, redirect, $pinia }) => {
+  const authStore = useAuth($pinia)
 
   $axios.onError((error) => {
-    // console.log(error)
-    // Issue with the current access token.
     if (error.response?.status === 401) {
       if (authStore.isLoggedIn) {
         authStore.logout()
--- packages/client/src/plugins/pinia.ts
+++ packages/client/src/plugins/pinia.ts
@@ -1,23 +1,9 @@
-import { useCookies } from '@/composables/useCookies'
-import { useAuth } from '@/store/useAuth'
-import { defineNuxtPlugin, onGlobalSetup } from '@nuxtjs/composition-api'
-import { PiniaPluginContext, PiniaStorePlugin } from 'pinia'
-import {
-  COOKIE_CALENDAR_TYPE,
-  COOKIE_COMPLETE,
-  COOKIE_JWT,
-} from '../utils/constants'
-
-export type StateStatus = 'Idle' | 'Loading' | 'Error'
-
-export interface StateError {
-  url?: string
-  status?: number
-  message: string
-}
+import { StateError, StateStatus } from '@/types/state.interface'
+import { defineNuxtPlugin } from '@nuxtjs/composition-api'
+import { PiniaPluginContext } from 'pinia'
 
 declare module 'pinia' {
-  export interface PiniaCustomProperties {
+  export interface PiniaCustomStateProperties {
     status: StateStatus
     error: StateError | null
     isLoading: boolean
@@ -46,67 +32,28 @@ const wrap = <F extends Procedure>(fn: F, store: StoreType) => {
   return wrappedFn
 }
 
-const plugin: PiniaStorePlugin = ({ store, options }) => {
-  // TODO: Disabled until https://github.com/posva/pinia/discussions/497 is resolved.
-  //
-  // if (!Object.prototype.hasOwnProperty.call(store.$state, 'status')) {
-  //   const statusRef = ref('Idle')
-  //   set(store.$state, 'status', statusRef)
-  //   set(store, 'status', statusRef)
+export default defineNuxtPlugin(({ pinia }) => {
+  pinia.use(({ store, options }) => {
+    // Disabled until https://github.com/posva/pinia/discussions/497 is resolved.
+    // if (!Object.hasOwnProperty.call(store.$state, 'status')) {
+    //   const statusRef = ref('Idle')
+    //   set(store.$state, 'status', statusRef)
+    //   set(store, 'status', statusRef)
 
-  //   const loadingRef = computed(() => store.status === 'Loading')
-  //   set(store.$state, 'isLoading', loadingRef)
-  //   set(store, 'isLoading', loadingRef)
-  // }
-
-  if (
-    !options.actions ||
-    !Object.prototype.hasOwnProperty.call(store.$state, 'status')
-  )
-    return
-
-  return Object.keys(options.actions).reduce(
-    (result: Record<string, Procedure>, action) => {
-      result[action] = wrap(store[action], store)
-
-      return result
-    },
-    {}
-  )
-}
-
-export default defineNuxtPlugin(({ pinia, $axios }) => {
-  pinia.use(plugin)
-
-  onGlobalSetup(async () => {
-    // const auth = useAuth()
-    // const cookies = useCookies()
-    // const token = cookies.get(COOKIE_JWT)
-    // const complete = cookies.get(COOKIE_COMPLETE) || false
-    // const calendarType = cookies.get(COOKIE_CALENDAR_TYPE)
-    // console.log(
-    //   `Global Setup [${token.substr(token.length - 5)}] [${complete}]`
-    // )
-    // if (calendarType) {
-    //   auth.settings.calendarType = calendarType
+    //   const loadingRef = computed(() => store.status === 'Loading')
+    //   set(store.$state, 'isLoading', loadingRef)
+    //   set(store, 'isLoading', loadingRef)
     // }
-    // if (!token) return
-    // cookies.set(COOKIE_JWT, token)
-    // cookies.set(COOKIE_COMPLETE, complete)
-    // auth.token = token
-    // auth.complete = complete
-    // $axios.setToken(token, 'Bearer')
-    // if (complete) {
-    //   await auth.getMyUser()
-    // } else {
-    //   await auth.getMyAccount()
-    // }
-    // if (auth.error?.status === 401) {
-    //   auth.token = null
-    //   auth.complete = false
-    //   cookies.remove(COOKIE_JWT)
-    //   cookies.remove(COOKIE_COMPLETE)
-    //   $axios.setToken(false)
-    // }
+
+    if (!options.actions) return
+
+    return Object.keys(options.actions).reduce(
+      (actions: Record<string, Procedure>, action) => {
+        actions[action] = wrap(store[action], store)
+
+        return actions
+      },
+      {}
+    )
   })
 })
