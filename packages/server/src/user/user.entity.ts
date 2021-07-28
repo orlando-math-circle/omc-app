332650affde6b178d984a2026fad7d3a05e7574e
traywcr <55033722+traywcr@users.noreply.github.com>
2021-07-28 18:55:02 -0400

    Add attendance collection to user entity

--- packages/server/src/user/user.entity.ts
+++ packages/server/src/user/user.entity.ts
@@ -20,6 +20,7 @@ import { IndustryDto } from './dtos/industry.dto';
 import { Gender } from './enums/gender.enum';
 import { Grade } from './enums/grade.enum';
 import { ReminderFreq } from './enums/reminder-freq.enum';
+import { Attendance } from '@server/attendance/attendance.entity';
 
 @Entity()
 export class User extends BaseEntity<User, 'id'> {
@@ -152,4 +153,10 @@ export class User extends BaseEntity<User, 'id'> {
 
   @OneToMany(() => FileAttachment, (a) => a.user, { orphanRemoval: true })
   attachments = new Collection<FileAttachment>(this);
+
+  @OneToMany(() => Attendance, (h) => h.user, {
+    eager: false,
+    orphanRemoval: true,
+  })
+  attendances = new Collection<Attendance>(this);
 }
