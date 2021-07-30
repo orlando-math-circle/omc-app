107f839c0924f35fe4b3ca61ef51a6fc2dd5f42e
traywcr <55033722+traywcr@users.noreply.github.com>
2021-07-30 15:16:09 -0400

    Fix attendance entity declaration causing errors

--- packages/server/src/user/user.entity.ts
+++ packages/server/src/user/user.entity.ts
@@ -20,7 +20,7 @@ import { IndustryDto } from './dtos/industry.dto';
 import { Gender } from './enums/gender.enum';
 import { Grade } from './enums/grade.enum';
 import { ReminderFreq } from './enums/reminder-freq.enum';
-import { Attendance } from '@server/attendance/attendance.entity';
+import { Attendance } from '../attendance/attendance.entity';
 
 @Entity()
 export class User extends BaseEntity<User, 'id'> {
